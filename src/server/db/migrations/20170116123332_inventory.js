9d97c364368ff2d3d7c988f25cb8e83005397d94
Gina <gdebell@gmail.com>
2017-01-16 13:32:52 -0700

    migration created, seed create

---
+++ src/server/db/migrations/20170116123332_inventory.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('sneaker_table', function(table){
+    table.increments('id')
+    table.string('name').notNullable();
+    table.double('price').notNullable();
+    table.string('image_url').notNullable();
+    table.text('description').notNullable();
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('sneaker_table');
+};
